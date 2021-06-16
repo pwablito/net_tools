7b3e3ca0ada38fc865a7464a020521f020bd8395
pwablito <pwablito@gmail.com>
2021-06-16 00:55:04 -0600

    Initial Commit

---
+++ Shared/Net_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Net_ToolsApp.swift
+//  Shared
+//
+//  Created by Paul Spencer on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Net_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
